ea657fbf46e9b04a372a48d2a238ee893730f20b
Jayluu2202 <jaymin28panchal@gmail.com>
2025-09-03 18:35:28 +0530

    last changes before project change

---
+++ Candor/View/Accessories Group/Asset Add Form/addAssetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  addAssetViewController.swift
+//  Candor
+//
+//  Created by mac on 03/09/25.
+//
+
+import UIKit
+
+class addAssetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
